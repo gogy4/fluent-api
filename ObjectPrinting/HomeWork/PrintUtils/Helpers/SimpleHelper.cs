cff7d5995299f31f04e40cc107016cda2377012a
younggogy <younggogy@gmail.com>
2025-11-20 00:21:22 +0500

    Вынес хелпер для проверки IsSimple

---
+++ ObjectPrinting/HomeWork/PrintUtils/Helpers/SimpleHelper.cs
@@ -0,0 +1,15 @@
+namespace ObjectPrinting.HomeWork.PrintUtils.Helpers;
+
+public static class SimpleHelper
+{
+    public static bool IsSimple(Type type)
+    {
+        return type.IsPrimitive
+               || type == typeof(string)
+               || type == typeof(decimal)
+               || type == typeof(DateTime)
+               || type == typeof(Guid)
+               || (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) &&
+                   IsSimple(type.GetGenericArguments()[0]));
+    }
+}
